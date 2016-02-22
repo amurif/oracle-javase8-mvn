1c64308e047712e0814d995c08883da3486ad0cb
FAM <amurif@hotmail.com>
2016-02-22 20:06:06 +0100

    Add recipe 7

---
+++ oca-sample/chapter02/src/main/java/recipe7/PoolCalculator.java
@@ -0,0 +1,7 @@
+package recipe7;
+
+/**
+ * Created by O26 on 22/02/2016.
+ */
+public class PoolCalculator {
+}
---
+++ oca-sample/chapter02/src/test/java/recipe7/PoolCalculatorTest.java
@@ -0,0 +1,28 @@
+package recipe7;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by O26 on 22/02/2016.
+ */
+public class PoolCalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testCalculateVolume() throws Exception {
+
+    }
+}
